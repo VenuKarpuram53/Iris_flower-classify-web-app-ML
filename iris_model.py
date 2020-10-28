429fb4c815cc8de35b2b52c30c52caa780f616ce
Venu Karpuram <73389195+VenuKarpuram53@users.noreply.github.com>
2020-10-28 12:43:52 +0530

    updated

--- iris_model.py
+++ iris_model.py
@@ -9,7 +9,6 @@ from sklearn import datasets
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression,LogisticRegression
-from sklearn.svm import SVC,LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 
@@ -20,17 +19,14 @@ y=iris.target
 
 x_train,x_test,y_train,y_test=train_test_split(X,y)
 lin_reg=LinearRegression()
-log_reg=LogisticRegression()
-support_VM=SVC(kernel='linear',probability=True)
+KM_clustering=LogisticRegression()
 decision_tree = DecisionTreeClassifier(max_depth = 6,random_state = 0,criterion = "entropy")
 
 lin_reg=lin_reg.fit(x_train,y_train)
-log_reg=log_reg.fit(x_train,y_train)
-support_VM=support_VM.fit(x_train,y_train)
+KM_clustering=KM_clustering.fit(x_train,y_train)
 decision_tree=decision_tree.fit(x_train, y_train)
 
 
 pickle.dump(lin_reg,open('neuron_model.pkl','wb'))
-pickle.dump(log_reg,open('log_model.pkl','wb'))
-pickle.dump(support_VM,open('svc_model.pkl','wb'))
+pickle.dump(KM_clustering,open('kmc_model.pkl','wb'))
 pickle.dump(decision_tree,open('decision_model.pkl','wb'))
